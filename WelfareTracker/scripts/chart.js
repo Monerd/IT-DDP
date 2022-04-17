bd5c4848aceb0b62a7779749bccbc0f851302975
Monerd <nemkovich.nikitka@mail.ru>
2022-04-17 18:41:58 +0300

    feat: add scripts for forecast & chart

---
+++ WelfareTracker/scripts/chart.js
@@ -0,0 +1,29 @@
+google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+  
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable([
+            ['Month', 'Balance'],
+            ['January', 5002],
+            ['February', 6010],
+            ['March', 7021],
+            ['April', 2400],
+            ['May', 10000],
+            ['June', 10000],
+            ['July', 10000],
+            ['August', 10000],
+            ['September', 12000],
+            ['October', 9000],
+            ['November', 10230],
+            ['December', 10780]
+          ]);
+  
+          var options = {
+            legend: { position: 'none' },
+            vAxis: { format: 'currency' },
+          };
+  
+          var chart = new google.visualization.LineChart(document.getElementById('chart'));
+  
+          chart.draw(data, options);
+        }
